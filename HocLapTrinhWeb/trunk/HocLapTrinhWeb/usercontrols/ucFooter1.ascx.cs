88a4bdad20f1d5467b75503b82ce424e4f4cd7dd
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@b83b56ce-4c6d-96a5-f199-e4272d1bcb41>
2013-05-21 15:31:54 +0000

    Tạm thời ngưng chạy Iframe

--- HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucFooter1.ascx.cs
+++ HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucFooter1.ascx.cs
@@ -5,9 +5,9 @@ public partial class usercontrols_ucFooter1 : HocLapTrinhWeb.UI.UCBase
     protected override void Page_Load(object sender, EventArgs e)
     {
         base.Page_Load(sender, e);
-        var rnd = new Random();
-        var i = rnd.Next(0, 3);
-        if (i == 1)
-            lrIframe.Text = "<iframe src='" + CurrentPage.UrlRoot + "/randomnews.aspx' width='0px' height='0px' style='display:none;'></iframe>";
+        //var rnd = new Random();
+        //var i = rnd.Next(0, 3);
+        //if (i == 1)
+        //    lrIframe.Text = "<iframe src='" + CurrentPage.UrlRoot + "/randomnews.aspx' width='0px' height='0px' style='display:none;'></iframe>";
     }
 }
