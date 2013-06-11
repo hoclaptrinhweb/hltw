ace27d2bff99c5fe9b329d5fa05ec98b04b731f9
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@b83b56ce-4c6d-96a5-f199-e4272d1bcb41>
2013-06-11 08:00:57 +0000

    Sử dụng auto news trong Xenforo thông qua RSS Tính năng hay

--- HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucRssDetail.ascx.cs
+++ HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucRssDetail.ascx.cs
@@ -12,12 +12,27 @@ public partial class usercontrols_ucRssDetail : HocLapTrinhWeb.UI.UCBase
             GetRSS(Convert.ToInt32(Request.QueryString["NewsTypeID"]));
     }
 
+    public int PageSize
+    {
+        get
+        {
+            try
+            {
+                return Request.QueryString["pagesize"] != null ? int.Parse(Request.QueryString["pagesize"]) : 50;
+            }
+            catch
+            {
+                return 50;
+            }
+        }
+    }
+
     private void GetRSS(int newsType)
     {
         try
         {
             var vnnNewsBll = new vnn_NewsBLL(CurrentPage.getCurrentConnection());
-            var dt = vnnNewsBll.GetAllNewsForGridView(0, 50, "Title,NewsID,Thumbnail,Brief,CreatedDate", newsType, 1, "", "", "");
+            var dt = vnnNewsBll.GetAllNewsForGridView(0, PageSize, "Title,NewsID,Thumbnail,Brief,CreatedDate,Content", newsType, 1, "", "", "");
             if (dt == null) return;
             var vnnNewsTypeBll = new vnn_NewsTypeBLL(CurrentPage.getCurrentConnection());
             var row = vnnNewsTypeBll.GetNewsTypeByID(newsType);
@@ -31,7 +46,7 @@ public partial class usercontrols_ucRssDetail : HocLapTrinhWeb.UI.UCBase
                     Description = row.IsDescriptionNull() ? "" : row.Description
                 };
             rss.AddRssChannel(channel);
-               
+
             foreach (var t in dt)
             {
                 try
@@ -43,14 +58,16 @@ public partial class usercontrols_ucRssDetail : HocLapTrinhWeb.UI.UCBase
                                 CurrentPage.UrlRoot + "/" + XuLyChuoi.ConvertToUnSign(row.NewsTypeName) + "/" +
                                 XuLyChuoi.ConvertToUnSign(t.Title) + "-hltw" + t.NewsID + ".aspx"
                         };
-
-                    item.Description = @"<a href='" + item.Link + "' alt='" + t.Title.Replace('"', ' ') + "'><img  border='0' align='left' src='" + CurrentPage.UrlRoot + "/images/w149-" + (t.IsThumbnailNull() ? "" : (t.Thumbnail.ToLower().Replace(Global.ImagesNews.ToLower(), ""))) + ".ashx' alt='" + t.Title + "'/></a>" + (t.Brief.Length > 300 ? t.Brief.Substring(0, 300) : t.Brief);
+                    if (string.IsNullOrEmpty(Request.QueryString["auto"]))
+                        item.Description = @"<a href='" + item.Link + "' alt='" + t.Title.Replace('"', ' ') + "'><img  border='0' align='left' src='" + CurrentPage.UrlRoot + "/images/w149-" + (t.IsThumbnailNull() ? "" : (t.Thumbnail.ToLower().Replace(Global.ImagesNews.ToLower(), ""))) + ".ashx' alt='" + t.Title + "'/></a>" + (t.Brief.Length > 300 ? t.Brief.Substring(0, 300) : t.Brief);
+                    else
+                        item.Description = @"<p style='text-align: center;'><img  border='0' align='left' src='" + CurrentPage.UrlRoot + "/" + (t.IsThumbnailNull() ? "" : t.Thumbnail) + "' alt='" + t.Title + "'/></p><br/>" + t.Brief + "<br/>" + t.Content;
                     item.pubDate = t.CreatedDate.ToString("r");
                     rss.AddRssItem(item);
                 }
                 catch (Exception)
                 {
- 
+
                 }
             }
             Response.Clear();
@@ -60,7 +77,7 @@ public partial class usercontrols_ucRssDetail : HocLapTrinhWeb.UI.UCBase
         }
         catch (Exception ex)
         {
- 
+
         }
     }
 }
