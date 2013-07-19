da608b842afc23d04c73f5bf33e314596ad69cf1
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@b83b56ce-4c6d-96a5-f199-e4272d1bcb41>
2013-07-19 07:34:31 +0000

    Winservice dùng để lấy tự động

---
+++ HocLapTrinhWeb/trunk/AutoNews/AutoNewService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AutoNews
+{
+    partial class AutoNewService : ServiceBase
+    {
+        static void Main(string[] args)
+        {
+#if(!DEBUG)
+            {
+                ServiceBase.Run(new AutoNewService());
+            }
+#else
+            {
+                AutoNewService service = new AutoNewService();
+                service.OnStart(null);
+                System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+            }
+#endif
+        }
+
+        public AutoNewService()
+        {
+            InitializeComponent();
+
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.ServiceName = "AutoNewService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                var path = ConfigurationManager.AppSettings["Uploads"];
+                path += "\\nhatnam.txt";
+                var file = new StreamWriter(path, true);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
---
+++ HocLapTrinhWeb/trunk/AutoNews/ProjectInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace AutoNews
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+
+            // Instantiate and configure a ServiceInstaller
+            service = new ServiceInstaller();
+            service.DisplayName = "AutoNewService";
+            service.ServiceName = "AutoNewService";
+            service.StartType = ServiceStartMode.Automatic;
+
+            // Add both the ServiceProcessInstaller and ServiceInstaller to
+            // the Installer collection, which is inherited from the
+            // Installer base class
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
