3155080ca0fe74192e2bc0a714eab99c894a5141
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@b83b56ce-4c6d-96a5-f199-e4272d1bcb41>
2013-11-28 14:03:11 +0000

    Tạo được dynamic dữ liệu xuống View

--- HocLapTrinhWeb/trunk/MvcHocLapTrinhWeb/Controllers/HomeController.cs
+++ HocLapTrinhWeb/trunk/MvcHocLapTrinhWeb/Controllers/HomeController.cs
@@ -9,14 +9,24 @@ namespace MvcHocLapTrinhWeb.Controllers
 {
     public class HomeController : Controller
     {
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Message = "Tập làm quen với MVC";
+        //    HLTWDB h = new HLTWDB();
+        //    var query = from n in h.tbl_Newss
+        //                orderby  Guid.NewGuid()
+        //                select n;
+        //    return View(query.Skip(0).Take(10).ToList());
+        //}
+
         public ActionResult Index()
         {
             ViewBag.Message = "Tập làm quen với MVC";
             HLTWDB h = new HLTWDB();
-            var query = from n in h.tbl_Newss
-                        orderby  Guid.NewGuid()
-                        select n;
-            return View(query.Skip(0).Take(10).ToList());
+            dynamic query = (from n in h.tbl_Newss
+                             orderby Guid.NewGuid()
+                             select new { Title = n.Title, Brief = n.Brief , NewsTypeName = n.tbl_News_Types.NewsTypeName }).Skip(0).Take(10).AsEnumerable().Select(c => c.ToExpando());
+            return View(query);
         }
 
         public ActionResult About()
---
+++ HocLapTrinhWeb/trunk/MvcHocLapTrinhWeb/Models/DynamicViewPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace MvcHocLapTrinhWeb.Models
+{
+    public static class impFunctions
+    {
+        public static ExpandoObject ToExpando(this object anonymousObject)
+        {
+            IDictionary<string, object> anonymousDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(anonymousObject);
+            IDictionary<string, object> expando = new ExpandoObject();
+            foreach (var item in anonymousDictionary)
+                expando.Add(item);
+            return (ExpandoObject)expando;
+        }
+    }
+}
