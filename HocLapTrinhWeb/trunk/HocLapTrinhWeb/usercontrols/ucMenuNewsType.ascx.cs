b43dc945f9466bac25dd78728b6ccde6214532c0
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@b83b56ce-4c6d-96a5-f199-e4272d1bcb41>
2014-01-17 09:04:17 +0000

    Sử dụng cơ chế Cache để trải nghiệm thử nó có nhanh hơn không - Tại vps không mạnh cho lắm

--- HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucMenuNewsType.ascx.cs
+++ HocLapTrinhWeb/trunk/HocLapTrinhWeb/usercontrols/ucMenuNewsType.ascx.cs
@@ -7,8 +7,13 @@ public partial class usercontrols_ucMenuNewsType : HocLapTrinhWeb.UI.UCBase
     protected override void Page_Load(object sender, EventArgs e)
     {
         base.Page_Load(sender, e);
-        var vnnNewsTypeBll = new vnn_NewsTypeBLL(CurrentPage.getCurrentConnection());
-        var dt = vnnNewsTypeBll.GetDataByParentID("NewsTypeName,NewsTypeID,TotalNews,ImageURL", -1);
+        var dt = (vnn_dsHocLapTrinhWeb.vnn_vw_NewsTypeDataTable)Cache["dataMenu"];
+        if (dt == null)
+        {
+            var vnnNewsTypeBll = new vnn_NewsTypeBLL(CurrentPage.getCurrentConnection());
+            Cache.Insert("dataMenu", vnnNewsTypeBll.GetDataByParentID("NewsTypeName,NewsTypeID,TotalNews,ImageURL", -1), null, DateTime.Now.AddDays(10), System.Web.Caching.Cache.NoSlidingExpiration);
+            dt = (vnn_dsHocLapTrinhWeb.vnn_vw_NewsTypeDataTable)Cache["dataMenu"];
+        }
         rpNewsType.DataSource = dt;
         rpNewsType.DataBind();
     }
